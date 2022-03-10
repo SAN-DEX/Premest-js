10f72895898df4b354d33e5e0d038312017ad661
Sandra Aidoo <nanasandra054@gmail.com>
2022-03-10 09:28:20 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+let firstName = "John";
+let secondName = "Doe";
+let age = 21;
+console.log(firstName);
+console.log(secondName);
+console.log(age);
+console.log("My name is " + firstName + " " + secondName);
+
+let Age = "2";
+let currentAge = 1;
+
+console.log("I am " + Age + currentAge + " years old. ");
+console.log(`My name is ${firstName} ${secondName}. I am ${age} years old.`);
+
+let fruit = ["Mango", "Pineapple", "Orange", "Apple"];
+console.log(fruit);
+console.log(fruit.length);
+console.log(fruit[1]);
+console.log(fruit.push("Banana"));
+console.log(fruit);
+console.log(fruit.pop());
+
+console.log(fruit.unshift("Pear"));
+console.log(fruit);
+console.log(fruit.shift("Pear"));
+console.log(fruit);
+
+console.log(fruit.includes("Orange"));
+console.log(fruit);
+console.log(fruit.includes("Rice"));
+console.log(fruit);
+
+console.log(fruit.join(""));
+console.log(fruit);
+console.log(fruit.join("_"));
+console.log(fruit);
+console.log(fruit.join(" "));
+console.log(fruit);
